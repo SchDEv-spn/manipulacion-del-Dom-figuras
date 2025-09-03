84ca862b3c9ce955572ee6a5e0504bcfe18327d5
schneider manrique <schneider.manrique@gmai.com>
2025-09-03 15:41:05 -0500

    agregue la funcion estrella

--- js/main.js
+++ js/main.js
@@ -5,10 +5,21 @@ function circulo() {
     btnCircle.addEventListener("click", () => {
         figure.style.borderRadius = "50%";
         figure.style.background = "green";
+        
     });
 }
 
+function estrella() {
+    const figure = document.getElementById("figure");
+    const btnStar = document.getElementById("btnStar");
 
+    btnStar.addEventListener("click", () => {
+        figure.style.clipPath = "polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, " +
+                                "79% 91%, 50% 70%, 21% 91%, 32% 57%, " +
+                                "2% 35%, 39% 35%)";
+        figure.style.background = "green";
+    });
+}
 
 
 
